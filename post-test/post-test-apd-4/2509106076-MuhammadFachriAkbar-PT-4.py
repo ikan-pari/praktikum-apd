3c10e049d5902a767d11b46a07317564bb99bc6b
ikan-pari <muhammadfachriakbar1@gmail.com>
2025-10-07 16:07:36 +0700

    up filr

--- post-test/post-test-apd-4/2509106076-MuhammadFachriAkbar-PT-4.py
+++ post-test/post-test-apd-4/2509106076-MuhammadFachriAkbar-PT-4.py
@@ -1,14 +1,19 @@
-
-
 print("===== SELAMAT DATANG DI BIOSKOP XX0 =====")
 
-nama_pengguna = "Fiantika Atha Wardhani"
-nim_pengguna = "2511016030"
-
+nama_pengguna = "Muhammad Fachri Akbar"
+nim_pengguna = "2509106076"
 percobaan = 0
 login_berhasil = False
+pilihan = 0
+jenis_tiket = ""
+harga_tiket = 0
+jumlah_tiket = 0
+total_bayar = 0
+potongan = 0
+bonus = ""
+total_akhir = total_bayar - potongan
 
-while percobaan < 3:
+while percobaan < 3 and login_berhasil == False:
     print("\n=== LOGIN ===")
     nama_input = input("Masukkan Nama Pengguna: ")
     nim_input = input("Masukkan NIM: ")
@@ -16,16 +21,13 @@ while percobaan < 3:
     if nama_input == nama_pengguna and nim_input == nim_pengguna:
         print("\nLogin Berhasil! Selamat datang", nama_pengguna)
         login_berhasil = True
-        break
     else:
-        percobaan = percobaan + 1
+        percobaan += 1
         print("Login Gagal! Percobaan ke-", percobaan)
 
 if login_berhasil == False:
-    print("\nAnda telah gagal login sebanyak 3 kali. Silakan coba lagi nanti..")
+    print("\nLogin ditolak. Anda telah gagal login sebanyak 3 kali.")
 else:
-    
-    pilihan = 0
     while pilihan != 4:
         print("\n=== MENU PEMBELIAN TIKET BIOSKOP XX0 ===")
         print("1. Tiket Reguler  - Rp 50.000")
@@ -35,52 +37,39 @@ else:
 
         pilihan = int(input("Pilih jenis tiket (1-4): "))
 
-        if pilihan == 1:
-            jenis_tiket = "Reguler"
-            harga_tiket = 50000
-        elif pilihan == 2:
-            jenis_tiket = "VIP"
-            harga_tiket = 100000
-        elif pilihan == 3:
-            jenis_tiket = "VVIP"
-            harga_tiket = 150000
+        if pilihan == 1 or pilihan == 2 or pilihan == 3:
+            if pilihan == 1:
+                jenis_tiket = "Reguler"
+                harga_tiket = 50000
+            elif pilihan == 2:
+                jenis_tiket = "VIP"
+                harga_tiket = 100000
+            elif pilihan == 3:
+                jenis_tiket = "VVIP"
+                harga_tiket = 150000
+
+            jumlah_tiket = int(input("Masukkan jumlah tiket yang ingin dibeli: "))
+            total_bayar = harga_tiket * jumlah_tiket
+            potongan = total_bayar * 0.12 if total_bayar >= 300000 else total_bayar * 0.08 if total_bayar >= 200000 else 0
+            bonus = "Poster Film Eksklusif" if total_bayar >= 150000 and total_bayar < 200000 else ""
+            total_akhir = total_bayar - potongan
+
+            print("\n===== STRUK PEMBELIAN =====")
+            print("Nama Pembeli   :", nama_pengguna)
+            print("Jenis Tiket    :", jenis_tiket)
+            print("Jumlah Tiket   :", jumlah_tiket)
+            print("Total Bayar    : Rp", total_bayar)
+
+            if potongan > 0:
+                print("Potongan Harga : Rp", int(potongan))
+                print("Total Akhir    : Rp", int(total_akhir))
+            elif bonus != "":
+                print("Bonus          :", bonus)
+            else:
+                print("Tidak ada potongan atau bonus.")
+            print("=============================")
+
         elif pilihan == 4:
             print("\nTerima kasih telah menggunakan layanan Bioskop XX0!")
-            break
         else:
             print("Pilihan tidak valid, silakan coba lagi.")
-            continue
-
-        jumlah_tiket = int(input("Masukkan jumlah tiket yang ingin dibeli: "))
-
-        total_bayar = 0
-        for i in range(jumlah_tiket):
-            total_bayar = total_bayar + harga_tiket
-
-        potongan = 0
-        bonus = ""
-
-        if total_bayar >= 300000:
-            potongan = total_bayar * 0.12
-        elif total_bayar >= 200000 and total_bayar < 300000:
-            potongan = total_bayar * 0.08
-        elif total_bayar >= 150000 and total_bayar < 200000:
-            bonus = "Poster Film Eksklusif"
-
-        total_akhir = total_bayar - potongan
-
-        print("\n===== STRUK PEMBELIAN =====")
-        print("Nama Pembeli   :", nama_pengguna)
-        print("Jenis Tiket    :", jenis_tiket)
-        print("Jumlah Tiket   :", jumlah_tiket)
-        print("Total Bayar    : Rp", total_bayar)
-
-        if potongan > 0:
-            print("Potongan Harga : Rp", int(potongan))
-            print("Total Akhir    : Rp", int(total_akhir))
-        elif bonus != "":
-            print("Bonus          :", bonus)
-        else:
-            print("Tidak ada potongan atau bonus.")
-
-        print("=============================")
