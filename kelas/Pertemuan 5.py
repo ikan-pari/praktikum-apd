9b3ec804e02bd42a2b832bdec93f23d2e4f17116
ikan-pari <muhammadfachriakbar1@gmail.com>
2025-10-15 10:12:49 +0700

    up file

---
+++ kelas/Pertemuan 5.py
@@ -0,0 +1,15 @@
+# #praktikum = ["Mahasiswa", 20, True, 45.10, ["APD", 25]]
+# #
+# #print(praktikum)
+
+# menkrep = ["sword","shield","armor"
+#             "potion","elixir"["healing", "mana"]]
+# menkrep
+# print(menkrep)
+
+anakB2 = ['Zuhri', 'Pahri', 'Bang Pernan', ['Udin'], 'Asep']
+print(anakB2)
+del anakB2[2]
+print(anakB2)
+anakB2.remove('Udin')
+print(anakB2)
